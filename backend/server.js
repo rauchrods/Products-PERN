cd06cf0631db93eb5326722e0a05185f2b71ec8c
rauchrods <rauchrodrigues@gmail.com>
2025-05-12 16:51:20 +0530

    added cluster worker threads

--- backend/server.js
+++ backend/server.js
@@ -6,26 +6,50 @@ import productRoutes from "./routes/productRoutes.js";
 import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import rateLimitingAndBotProtection from "./middleware/rateLimitingAndBotProtection.js";
-
-
+import cluster from "cluster";
+import os from "os";
 
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT;
+if (cluster.isPrimary) {
+  const numCPUs = os.cpus().length;
+  console.log(`Primary ${process.pid} is running`);
+  console.log(`Launching ${numCPUs} workers`);
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(helmet()); //helmet is a security middleware that sets various HTTP headers to help protect your app from common web vulnerabilities
-app.use(morgan("dev"));
+  // Listen for dying workers and replace them
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(
+      `Worker ${worker.process.pid} died with code: ${code} and signal: ${signal}`
+    );
+    console.log("Starting a new worker");
+    cluster.fork();
+  });
+} else {
+  console.log(`Worker ${process.pid} started`);
 
-// apply arcjet middleware (rate limiting and bot protection)
-app.use(rateLimitingAndBotProtection);
+  const app = express();
+  const PORT = process.env.PORT;
 
-app.use("/api/products", productRoutes);
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false })); // a middleware used to upload files to the server
+  app.use(cors());
+  app.use(helmet()); //helmet is a security middleware that sets various HTTP headers to help protect your app from common web vulnerabilities
+  app.use(morgan("dev"));
 
-initDB().then(() => {
-  console.log("Database initialized");
-  app.listen(PORT, () => console.log(`Server running on port ${PORT} and env is ${process.env.NODE_ENV}`));
-});
+  // apply arcjet middleware (rate limiting and bot protection)
+  app.use(rateLimitingAndBotProtection);
+
+  app.use("/api/products", productRoutes);
+
+  initDB().then(() => {
+    console.log("Database initialized");
+    app.listen(PORT, () =>
+      console.log(
+        `Server running on port ${PORT} and env is ${process.env.NODE_ENV}`
+      )
+    );
+  });
+}
